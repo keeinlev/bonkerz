0e8d8a3dfff267bccec1a4e61167ec567c2b6763
keeinlev <75710848+keeinlev@users.noreply.github.com>
2021-03-19 02:26:51 -0400

    big branding changes and deploy

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'https://testify-smiley-face.herokuapp.com/posts'//'http://localhost:5000/posts';
+const url = 'https://freeee-flow.herokuapp.com/posts'//'http://localhost:5000/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
