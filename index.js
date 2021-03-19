30a4d2755d53ff84555772e0b94aed1ed4cdb00c
keeinlev <75710848+keeinlev@users.noreply.github.com>
2021-03-19 02:36:28 -0400

    local test

--- index.js
+++ index.js
@@ -27,12 +27,12 @@ mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnified
 
 mongoose.set('useFindAndModify', false);
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-    app.get('*', (req,res)=>{
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-}
+//if (process.env.NODE_ENV === 'production') {
+app.use(express.static('client/build'));
+//    app.get('*', (req,res)=>{
+//        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//    })
+//}
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
