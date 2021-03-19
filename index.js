26ed74554b87161c68c4cd489042613918631324
keeinlev <75710848+keeinlev@users.noreply.github.com>
2021-03-19 02:04:41 -0400

    deploy attempt 2

--- index.js
+++ index.js
@@ -15,10 +15,6 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-app.get('/', (req, res) => {
-    res.send('Hello to testify:) API');
-})
-
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
