3f3cb614f922931aaae4f098daaffbe55e065660
keeinlev <75710848+keeinlev@users.noreply.github.com>
2021-03-19 02:35:44 -0400

    about and rename again

--- client/src/App.js
+++ client/src/App.js
@@ -20,8 +20,12 @@ const App = () => {
     return (
         <Container maxwidth="lg">
             <AppBar className={ classes.appBar } position="static" color="inherit">
-                <Typography className={ classes.heading } variant="h2" align="center">:freeFlow:</Typography>
+                <Typography className={ classes.heading } variant="h2" align="center">:BoNKERZ:</Typography>
                 {/*<img className={ classes.image } src={memories} alt="memories" height="60" />*/}
+            </AppBar>
+            <AppBar className={ classes.appBar } position="static" color="inherit">
+                <Typography className={ classes.Posts } variant="h2" align="center">About</Typography>
+                <Typography variant="body2" align="center" component="p">Welcome to BoNKERZ! This is the fast-paced social media app with no rules. Create a Thread in the form and it will appear to all users. The catch? Anyone can edit your post. Oh, and, only 20 posts will ever be on the site. Once it reaches past that limit, you can kiss that Thread goodbye. Have fun!</Typography>
             </AppBar>
             <Grow in>
                 <Container>
